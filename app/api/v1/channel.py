fd626ee67780ef3d95e8f83f84542520af2f1544
Mikhail Khromov <khromov05@gmail.com>
2023-09-12 11:33:53 +0300

    fix channel api routes

--- app/api/v1/channel.py
+++ app/api/v1/channel.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter, HTTPException, Depends
 from tortoise.transactions import atomic
 
-from app.api.v1.schemas import ChannelCreate, ChannelResponse
+from app.api.v1.schemas import ChannelType, ChannelCreateResponse
 from app.db.models import Channel
 
 router = APIRouter()
 
 
-@router.post("/channel", response_model=ChannelResponse)
+@router.post("/channel", response_model=ChannelCreateResponse)
 @atomic()
-async def add_channel(channel_data: ChannelCreate):
+async def add_channel(channel_data: ChannelType):
     channel = await Channel.create(**channel_data.model_dump())
     return channel
 
 
-@router.get("/channel/{internal_id}", response_model=ChannelResponse)
+@router.get("/channel/{internal_id}", response_model=ChannelCreateResponse)
 async def get_channel(internal_id: str):
     channel = await Channel.get_or_none(internal_id=internal_id)
     if not channel:
@@ -22,9 +22,9 @@ async def get_channel(internal_id: str):
     return channel
 
 
-@router.put("/channel/{internal_id}", response_model=ChannelResponse)
+@router.put("/channel/{internal_id}", response_model=ChannelCreateResponse)
 @atomic()
-async def update_channel(internal_id: str, channel_data: ChannelCreate):
+async def update_channel(internal_id: str, channel_data: ChannelType):
     channel = await Channel.get_or_none(internal_id=internal_id)
     if not channel:
         raise HTTPException(status_code=404, detail="Channel not found")
