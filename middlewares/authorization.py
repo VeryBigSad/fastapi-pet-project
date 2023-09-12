643a4881f868cd10ae6ed3daf93f1885ac1311db
Mikhail Khromov <khromov05@gmail.com>
2023-09-12 11:39:47 +0300

    fix middleware exception raising

--- middlewares/authorization.py
+++ middlewares/authorization.py
@@ -1,4 +1,5 @@
 from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
 
 valid_access_keys = ["key1", "key2"]  # TODO: replace with on-startup keys
 
@@ -7,7 +8,8 @@ async def authorization_middleware(request: Request, call_next):
     access_key = request.headers.get("X-ACCESS-KEY")
 
     if access_key not in valid_access_keys:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        return JSONResponse(status_code=401, content={"detail": "Unauthorized"})
+        # raise HTTPException(status_code=401, detail="Unauthorized")
 
     response = await call_next(request)
     return response
