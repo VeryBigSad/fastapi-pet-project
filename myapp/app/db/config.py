6fb9db5901945572605c5986140de1f96902351d
Mikhail Khromov <khromov05@gmail.com>
2023-09-12 10:50:03 +0300

    sqlite3 database with tortoise set up & models

---
+++ myapp/app/db/config.py
@@ -0,0 +1,22 @@
+from tortoise import Tortoise
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": "sqlite://db.sqlite3",
+    },
+    "apps": {
+        "models": {
+            "models": ["app.db.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
+
+
+async def init_db():
+    await Tortoise.init(config=TORTOISE_ORM)
+    await Tortoise.generate_schemas()
+
+
+async def close_db():
+    await Tortoise.close_connections()
---
+++ myapp/app/db/models.py
@@ -0,0 +1,16 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class Bot(Model):
+    internal_id = fields.UUIDField(pk=True)
+    token = fields.CharField(max_length=255)
+    title = fields.CharField(max_length=20)
+    tmp_chat_id = fields.IntField()
+
+
+class Channel(Model):
+    internal_id = fields.UUIDField(pk=True)
+    bot_uuid = fields.UUIDField()
+    channel_id = fields.IntField()
+    channel_name = fields.CharField(max_length=20)
